3e70edc410545ca5d744919cb705b85daf30d386
AdrianLayton <AdrianLayton@gmail.com>
2017-12-17 05:30:36 -0500

    RF7

--- app.js
+++ app.js
@@ -1,15 +1,36 @@
 const express = require ('express');
 const bodyParser = require('body-parser');
 const AWS = require('aws-sdk');
-const credResponse = new AWS.EC2MetadataCredentials();
-credResponse.refresh();
+const credResponse = new AWS.EC2MetadataCredentials({
+	  httpOptions: { timeout: 5000 }, 
+	  maxRetries: 10, 
+	  retryDelayOptions: { base: 200 }
+});
+credResponse = credResponse.refresh();
+const credResponse1 = new AWS.MetadataService();
+credResponse1.request("latest/meta-data/iam/security-credentials/ServerDbCrud", function(err, data) {
+	if (data) {
+		console.log(data)
+	else {
+		console.log(err)
+	}
+	}
+});
 
 const app = express();
 
-AWS.config.update({
-  region: "us-east-1",
-  endpoint: "arn:aws:dynamodb:us-east-1:704381840790:table/testDb"
+AWS.config.getCredentials(function(err) {
+  if (err) console.log(err.stack); // credentials not loaded
+  else { 
+    console.log("Access Key:", AWS.config.credentials.accessKeyId);
+    // pass AWS.config.credentials properties to your SimpleDB code
+  }
 });
+
+// AWS.config.update({
+//   region: "us-east-1",
+//   endpoint: "arn:aws:dynamodb:us-east-1:704381840790:table/testDb"
+// });
 
 // AWS.config.credentials = new AWS.EC2MetadataCredentials();
 
@@ -20,4 +41,6 @@ app.get('/', (req,res) => {
 app.listen(3000,() => {
 	console.log('App has started on port 3000');
 	console.log(credResponse);
+	console.log(credResponse1);
+	console.log("Access Key:", AWS.config.credentials.accessKeyId)
 });
